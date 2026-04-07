b70a97c29173e055829a5623c9cb2a25bf2a2e92
smcuaresma <scuaresma214@gmail.com>
2026-04-07 15:19:59 +0000

    Tue, Apr 7, 2026, 11:19 AM -04:00

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Dinner-Dojo
+
+#Using this API: https://www.themealdb.com/api/json/v1/1/search.php?f=a we plan to generate recipe recommendations given different lists of ingredients, which will optimize the recipes and list meal options along with missing ingredients. 
---
+++ Test_Fridge.py
@@ -0,0 +1,4 @@
+ingredients = {'test1':["Romano Pepper","Lamb Mince","Sunflower Oil"],
+               'test2':["Potatoes","Olive Oil","Onion","Paprika","Scallions"]}
+#these are test lists of ingredients which shoudl produce different outputs. For example, test1 should recommend Adana
+# Kebab and test 2 should recommoned air fryer patatas bravas
