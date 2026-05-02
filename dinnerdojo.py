b8900e22d49ccb3b05f7f3214b9bc4e7a97bf184
smcuaresma <scuaresma214@gmail.com>
2026-05-02 20:21:22 +0000

    Sat, May 2, 2026, 4:21 PM -04:00

--- dinnerdojo.py
+++ dinnerdojo.py
@@ -107,8 +107,12 @@ if __name__ == "__main__":
         exit()
     matches, recipe_matched, recipe_missing = dinner_dojo(category,meals)
 
+    meal_str = matches[0]
+best_meal_name = matches[0]
+best_meal = next(m for m in meals if m["strMeal"] == best_meal_name)
+
+print("Thumbnail URL:", best_meal["strMealThumb"])
     print("\nBest suggestion:")
-    print("Thumbnail URL:", matches[0]["strMealThumb"])
     print(matches[0])
     print("You have these ingredients: ",recipe_matched[matches[0]])
     print("You are missing these ingredients: ",recipe_missing[matches[0]])
